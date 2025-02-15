18bedf88a610126d715318435f45afa364b90ed4
Ariane Mora <ariane.n.mora@gmail.com>
2025-02-15 14:05:28 -0800

    Added rxnf

---
+++ enzymetk/conda_envs/rxnfp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+conda create -n rxnfp --channel conda-forge python=3.8 -y
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+
+conda activate rxnfp
+conda install -c rdkit rdkit=2020.03.3 -y
+conda install -c tmap tmap -y
+pip install rxnfp

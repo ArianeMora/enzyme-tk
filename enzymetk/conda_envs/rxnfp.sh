cd168fc26f3ff598f8963b1f6d5e5a7619eb538e
Ariane Mora <ariane.n.mora@gmail.com>
2025-02-15 14:28:15 -0800

    Updated rxnfp

--- enzymetk/conda_envs/rxnfp.sh
+++ enzymetk/conda_envs/rxnfp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conda create -n rxnfp --channel conda-forge python=3.8 -y
+conda create -n rxnfp --channel conda-forge python=3.6 -y
 CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
 
@@ -8,3 +8,4 @@ conda activate rxnfp
 conda install -c rdkit rdkit=2020.03.3 -y
 conda install -c tmap tmap -y
 pip install rxnfp
+#pip install numpy==1.21
--- enzymetk/steps/embedchem_rxnfp_step.py
+++ enzymetk/steps/embedchem_rxnfp_step.py
@@ -6,7 +6,7 @@ import subprocess
 from pathlib import Path
 import logging
 import numpy as np
-
+from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -14,16 +14,17 @@ logger.setLevel(logging.INFO)
     
 class RxnFP(Step):
     
-    def __init__(self, value_col: str, num_threads: int):
+    def __init__(self, value_col: str, num_threads: int, bad_server=False):
         self.value_col = value_col
         self.num_threads = num_threads or 1
+        self.bad_server = bad_server
 
     def __execute(self, df: pd.DataFrame, tmp_dir: str) -> pd.DataFrame:
         output_filename = f'{tmp_dir}/rxnfp.pkl'
         input_filename = f'{tmp_dir}/input.csv'
         df.to_csv(input_filename, index=False)
         # Might have an issue if the things are not correctly installed in the same dicrectory 
-        result = subprocess.run(['python', Path(__file__).parent/'embedchem_rxnfp_run.py', '--out', output_filename, 
+        result = subprocess.run(['conda', 'run', '-n', 'rxnfp', 'python', Path(__file__).parent/'embedchem_rxnfp_run.py', '--out', output_filename, 
                                 '--input', input_filename, '--label', self.value_col], capture_output=True, text=True)
         if result.stderr:
             logger.error(result.stderr)
@@ -36,7 +37,7 @@ class RxnFP(Step):
             if self.num_threads > 1:
                 output_filenames = []
                 df_list = np.array_split(df, self.num_threads)
-                for df_chunk in df_list:
+                for df_chunk in tqdm(df_list, total=len(df_list)):
                     output_filenames.append(self.__execute(df_chunk, tmp_dir))
                     
                 df = pd.DataFrame()

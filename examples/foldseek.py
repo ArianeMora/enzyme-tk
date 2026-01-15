59fc760ff68508548e69c6fa7e7fb3e6bcd1d8fe
Ariane Mora <ariane.n.mora@gmail.com>
2026-01-15 13:43:05 +0000

    Updated database passing for foldseek

--- examples/foldseek.py
+++ examples/foldseek.py
@@ -6,12 +6,12 @@ import pandas as pd
 
 
 # id_col: str, seq_col: str, proteinfer_dir: str,
-output_dir = 'tmp/'
-rows = [['P0DP24_3', 'tmp/P0DP24/chai/P0DP24_3.cif'],
-        ['P0DP24_1', 'tmp/P0DP24/chai/P0DP24_1.cif']]
-df = pd.DataFrame(rows, columns=['id', 'pdbs'])
-# foldseek_dir: str, pdb_column_name: str, reference_database: str
-reference_database = '/disk4/share/software/foldseek/structures/pdb/pdb'
+# output_dir = 'tmp/'
+# rows = [['P0DP24_3', 'tmp/P0DP24/chai/P0DP24_3.cif'],
+#         ['P0DP24_1', 'tmp/P0DP24/chai/P0DP24_1.cif']]
+# df = pd.DataFrame(rows, columns=['id', 'pdbs'])
+# # foldseek_dir: str, pdb_column_name: str, reference_database: str
+# reference_database = '/disk4/share/software/foldseek/structures/pdb/pdb'
 #df << (FoldSeek('id', 'pdbs', reference_database) >> Save(f'{output_dir}pdb_files.pkl'))
 #df << (FoldSeek('id', 'pdbs', reference_database, method='cluster', tmp_dir='tmp/') >> Save(f'{output_dir}pdb_files.pkl'))
 
@@ -28,8 +28,10 @@ rows = [['AXE2_TALPU', 'query', 'MHSKFFAASLLGLGAAAIPLEGVMEKRSCPAIHVFGARETTASPGYG
         ['A0A0B8RHP0_LISMN', 'reference', 'MKKLLFLGDSVTDAGRDFENDRELGHGYVKIIADQLEQEDVTVINRGVSANRVADLHRRIEADAISLQPDVVTIMIGINDTWFSFSRWEDTSVTAFKEVYRVILNRIKTETNAELILMEPFVLPYPEDRKEWRGDLDPKIGAVRELAAEFGATLIPLDGLMNALAIKHGPTFLAEDGVHPTKAGHEAIASTWLEFTK']]
 df = pd.DataFrame(rows, columns=[id_col, label_col, seq_col])
 # foldseek_dir: str, pdb_column_name: str, reference_database: str
-reference_database = '/disk4/share/software/foldseek/structures/pdb/pdb'
+reference_database = '/mnt/labs/data/mora/data/pdb_foldseek/pdb'
 #df << (FoldSeek(id_col, seq_col, reference_database, query_type='seqs',  method='cluster') >> Save(f'{output_dir}pdb_files_seqs.pkl'))
 # foldseek_dir: str, pdb_column_name: str, reference_database: str
 # #df << (FoldSeek('id', 'pdbs', reference_database) >> Save(f'{output_dir}pdb_files.pkl'))
-df << (FoldSeek(id_col, seq_col, reference_database, query_type='seqs',  tmp_dir='tmp/') >> Save(f'{output_dir}pdb_files.pkl'))
+df << (FoldSeek(id_col, seq_col, reference_database, query_type='seqs', database_dir='pdb_database/', tmp_dir='tmp/') >> Save(f'{output_dir}pdb_files.pkl'))
+# For checking against a reference database that already exists
+df << (FoldSeek('id', 'pdbs', reference_database) >> Save(f'{output_dir}pdb_files.pkl'))
